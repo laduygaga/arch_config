be21eb091e949360eff8a355ed636f65c03e6fc3
laduygaga <laduygaga>
2021-08-10 14:19:18 +0700

    remove fillter in thepower.sh allUp.sh make it faster

--- scripts/allUp.sh
+++ scripts/allUp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 url="0x0.st"
-file=$(find $HOME -type f -not -path '*/\.git/*' -not -path '*/\.vim/*' -not -path '*/\venv/*'| dmenu -i -l 10)
+file=$(find $HOME -type f | dmenu -i -l 10)
 curl -sF"file=@$file" $url | xclip -selection c
 notify-send "Uploaded!"
 
