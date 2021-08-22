0d6a2ee169b2e02de662321881eca26443cb6a4f
abc.xyz <abc.xyz>
2021-08-22 23:45:28 +0700

    update nvim

--- scripts/allUp.sh
+++ scripts/allUp.sh
@@ -2,6 +2,5 @@
 
 url="0x0.st"
 file=$(find $HOME -type f | dmenu -i -l 10)
-curl -sF"file=@$file" $url | xclip -selection c
-notify-send "Uploaded!"
+[[ ! -z $file ]] && curl -sF"file=@$file" $url | xclip -selection c && notify-send "Uploaded!"
 
