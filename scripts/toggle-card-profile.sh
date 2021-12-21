e6fdb42cbca5b55ecdea30c081c439bf44a0c8b7
duynn <duynn@example.com>
2021-12-22 00:04:06 +0700

    update card index

--- scripts/toggle-card-profile.sh
+++ scripts/toggle-card-profile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[[ `pactl list | grep -C2 A2DP | grep 'Active Profile: a2dp_sink_sbc'` ]] && pacmd set-card-profile 6 headset_head_unit ||  pacmd set-card-profile 6 a2dp_sink_sbc
+[[ `pactl list | grep -C2 A2DP | grep 'Active Profile: a2dp_sink_sbc'` ]] && pacmd set-card-profile 1 headset_head_unit ||  pacmd set-card-profile 1 a2dp_sink_sbc
