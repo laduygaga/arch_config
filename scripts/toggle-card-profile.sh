a4198ec5168b255fd584718713344165456f7797
duynn <duynn@example.com>
2021-12-22 23:28:08 +0700

    update toggle-card-profile

--- scripts/toggle-card-profile.sh
+++ scripts/toggle-card-profile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-[[ `pactl list | grep -C2 A2DP | grep 'Active Profile: a2dp_sink_sbc'` ]] && pacmd set-card-profile 1 headset_head_unit ||  pacmd set-card-profile 1 a2dp_sink_sbc
+index=`pacmd list-cards  | grep index | tail -1 | awk -F ' ' '{print $2}'`
+[[ `pactl list | grep -C2 A2DP | grep 'Active Profile: a2dp_sink_sbc'` ]] && pacmd set-card-profile $index headset_head_unit ||  pacmd set-card-profile $index a2dp_sink_sbc
