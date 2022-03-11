9231f290ddf5da5653ca3ade46d5b1fec3251d3c
duynn <hotboyarsenal@gmail.com>
2022-03-11 14:19:01 +0700

    dwm focus on mouse

--- dwm/config.h
+++ dwm/config.h
@@ -129,8 +129,8 @@ static Key keys[] = {
     { MODKEY,               XK_f,           togglefullscr,      {0} },
     { MODKEY,               XK_0,           view,               {.ui = ~0 } },
     { MODKEY|ShiftMask,     XK_0,           tag,                {.ui = ~0 } },
-    // { MODKEY,            XK_comma,       focusmon,           {.i = -1 } },
-    // { MODKEY,            XK_period,      focusmon,           {.i = +1 } },
+    { Mod4Mask,             XK_Tab,			focusmon,           {.i = -1 } },
+    // { Mod4Mask,             XK_2,			focusmon,           {.i = +1 } },
     // { MODKEY|ShiftMask,  XK_comma,       tagmon,             {.i = -1 } },
     // { MODKEY|ShiftMask,  XK_period,      tagmon,             {.i = +1 } },
     TAGKEYS(                XK_1,                               0)
