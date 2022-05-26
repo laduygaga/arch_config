aaeba31399503c123a8e9cfc2e3304a36bc0e340
duynn <hotboyarsenal@gmail.com>
2022-05-26 08:53:11 +0700

    add eth to coin_price.sh

--- scripts/coin_price.sh
+++ scripts/coin_price.sh
@@ -3,8 +3,9 @@
 
 function get_btc_price() {
 	btc=`curl -s https://api.coinbase.com/v2/prices/BTC-USD/spot | jq -r '.data.amount'`
+	eth=`curl -s https://api.coinbase.com/v2/prices/ETH-USD/spot | jq -r '.data.amount'`
 	gods=`curl -s https://api.coinbase.com/v2/prices/GODS-USD/spot | jq -r '.data.amount'`
-	echo "BTC: $btc" "GODS: $gods" > /tmp/coin_price
+	echo "BTC: $btc" "ETH": "$eth" "GODS: $gods" > /tmp/coin_price
 }
 
 get_btc_price
