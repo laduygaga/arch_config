eba6033d8d0289d2df511d947541aff97cf300f6
duynn <hotboyarsenal@gmail.com>
2022-06-27 09:13:29 +0700

    update thepower, allup scripts

--- scripts/allUp.sh
+++ scripts/allUp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 url="0x0.st"
-file=$(find $HOME -type f | dmenu -i -l 10)
-curl -sF"file=@$file" $url | xclip -selection c
-notify-send "Uploaded!"
+
+[[ ! -z `command -v fd` ]]  && file=$(fd -HI | dmenu -i -l 10)
+
+[[ ! -z $file ]] && curl -sF"file=@$file" $url | xclip -selection c && notify-send "Uploaded!"
 
