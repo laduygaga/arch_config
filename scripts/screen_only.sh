febe47e0a7b869072cfbe25c3349d1114e0a1efd
duynn <duynn1@appota.com>
2023-08-04 10:33:08 +0700

    change size when record screen

--- scripts/screen_only.sh
+++ scripts/screen_only.sh
@@ -1,1 +1,1 @@
-yes | ffmpeg -f x11grab -video_size 1366x768 -i :0 -c:v libx264 -preset ultrafast ~/.trash/"`date "+%Y%b%d(%a)%I:%M%p"`_screen_only.mp4" 2> /dev/null
+yes | ffmpeg -f x11grab -video_size 1920x1080 -i :0 -c:v libx264 -preset ultrafast ~/.trash/"`date "+%Y%b%d(%a)%I:%M%p"`_screen_only.mp4" 2> /dev/null
