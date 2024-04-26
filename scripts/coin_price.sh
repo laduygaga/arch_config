1759061a0c7466c292e22f5f48d1ed171e8c3380
laduygaga <hotboyarsenal@gmail.com>
2024-04-26 11:37:25 +0700

    get strk price

--- scripts/coin_price.sh
+++ scripts/coin_price.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # program to get btc price from coinbase.com
 
-function get_coin_price() {
+function get_btc_price() {
 	btc=`curl -s https://api.coinbase.com/v2/prices/BTC-USD/spot | jq -r '.data.amount'`
+	# gods=`curl -s https://api.coinbase.com/v2/prices/GODS-USD/spot | jq -r '.data.amount'`
+	strk=`curl -s https://api.coinbase.com/v2/prices/STRK-USD/spot | jq -r '.data.amount'`
 	eth=`curl -s https://api.coinbase.com/v2/prices/ETH-USD/spot | jq -r '.data.amount'`
-	usd2vnd=`curl  -s https://v6.exchangerate-api.com/v6/52bc1d8fe1eddb9dc1a2ee4d/latest/USD | jq -r ".conversion_rates.VND"`
-	echo "BTC: $btc" "ETH: $eth" "USD: $usd2vnd" > /tmp/coin_price.txt
+	echo "BTC: $btc" "ETH: $eth" "STRK: $strk" > /tmp/coin_price.txt
 }
 
-get_coin_price
+get_btc_price
