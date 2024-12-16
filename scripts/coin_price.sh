ae2bd14bbfe2e44a6924521321bb80955fe71962
duynn <hotboyarsenal@gmail.com>
2024-12-16 20:42:03 +0700

    make dwm tabmode show bottom

--- scripts/coin_price.sh
+++ scripts/coin_price.sh
@@ -11,14 +11,15 @@
 # get price from binance instead
 function get_btc_price() {
 	btc=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
-	eth=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
+	# eth=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
 	# not=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=NOTUSDT | jq -r '.price' | awk '{printf "%.5f\n", $1}'`
-	pepe=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=PEPEUSDT | jq -r '.price' | awk '{printf "%.7f\n", $1}'`
-	io=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=IOUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
+	# pepe=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=PEPEUSDT | jq -r '.price' | awk '{printf "%.7f\n", $1}'`
+	# io=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=IOUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
 	sol=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
 	doge=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=DOGEUSDT | jq -r '.price' | awk '{printf "%.4f\n", $1}'`
 	ton=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=TONUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
-	echo "BTC: $btc" "ETH: $eth" "PEPE:$pepe" "IO:$io" "SOL:$sol" "DOGE:$doge" "TON:$ton" > /tmp/coin_price.txt
+	xrp=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=XRPUSDT | jq -r '.price' | awk '{printf "%.4f\n", $1}'`
+	echo "BTC: $btc" "SOL: $sol" "DOGE: $doge" "TON: $ton" "XRP: $xrp" > /tmp/coin_price.txt
 }
 
 
