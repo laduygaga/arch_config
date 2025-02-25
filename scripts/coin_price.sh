fbcf5d83a6b0c87dc38f8b14f36f61c3f3440519
duynn <hotboyarsenal@gmail.com>
2025-02-25 10:06:12 +0700

    update shortcut for translate-shell

--- scripts/coin_price.sh
+++ scripts/coin_price.sh
@@ -14,13 +14,13 @@ function get_btc_price() {
 	# eth=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
 	# not=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=NOTUSDT | jq -r '.price' | awk '{printf "%.5f\n", $1}'`
 	# pepe=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=PEPEUSDT | jq -r '.price' | awk '{printf "%.7f\n", $1}'`
-	# io=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=IOUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
+	ton=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=TONUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
+	io=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=IOUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
 	# sol=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
 	# doge=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=DOGEUSDT | jq -r '.price' | awk '{printf "%.4f\n", $1}'`
-	# ton=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=TONUSDT | jq -r '.price' | awk '{printf "%.2f\n", $1}'`
 	# xrp=`curl -s https://api.binance.com/api/v3/ticker/price?symbol=XRPUSDT | jq -r '.price' | awk '{printf "%.4f\n", $1}'`
 	# echo "BTC: $btc" "SOL: $sol" "DOGE: $doge" "TON: $ton" "XRP: $xrp" > /tmp/coin_price.txt
-	echo "BTC: $btc" > /tmp/coin_price.txt
+	echo "BTC: $btc" "TON: $ton" "IO: $io" > /tmp/coin_price.txt
 }
 
 
