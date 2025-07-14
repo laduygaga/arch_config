7f1029013374eb2183f270bbdfa974e7bf0e8dcc
duynn <duynn@gmail.com>
2025-07-14 14:52:02 +0700

    use tesseract to copy text from image instead of textsnatch

--- scripts/thePower.sh
+++ scripts/thePower.sh
@@ -6,7 +6,7 @@
 [[ ! -z $file ]] &&
 case $(file --mime-type "$file" -b) in
 	text/html) $BROWSER "$file" >/dev/null 2>&1 &;;
-	text/*) sh -c "st -e nvim "$file"";;
+	text/*) sh -c "alacritty -e nvim "$file"";;
 	audio/*) mpv "$file" >/dev/null 2>&1 &;;
 	video/*) mpv "$file" >/dev/null 2>&1 &;;
 	image/gif) mpv --loop=inf "$file";;
