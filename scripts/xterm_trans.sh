78b33a8067db010226a356b2e3c6c8ce9c87f834
duynn <duynn@gmail.com>
2026-01-26 14:56:39 +0700

    update trans

--- scripts/xterm_trans.sh
+++ scripts/xterm_trans.sh
@@ -1,2 +1,4 @@
-xterm -hold  -geometry 50x20+0+590 -e trans :vi -b "`xclip -o`"
-# xterm -hold -e _trans "`xclip -o`"
+# xterm -hold  -geometry 50x20+0+590 -e trans :vi -b "`xclip -o`"
+
+# Improved version to handle long text better
+xterm -T "xterm_trans.sh" -geometry 50x20+0+590 -e bash -c "xclip -o | trans :vi -b | fmt -w 50; echo ''; read -n 1 -s -p 'Press any key to close...'"
