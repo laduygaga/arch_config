ad6b2f4fa6a8bb9861fbba8df03bd824f6517fe5
duynn <hotboyarsenal@gmail.com>
2026-02-01 12:12:34 +0700

    allUp.sh now not find hidden and ignored files

--- scripts/allUp.sh
+++ scripts/allUp.sh
@@ -2,7 +2,8 @@
 
 url="0x0.st"
 
-[[ ! -z `command -v fd` ]]  && file=$(fd -HI | dmenu -i -l 10)
+# [[ ! -z `command -v fd` ]]  && file=$(fd -HI | dmenu -i -l 10)
+[[ ! -z `command -v fd` ]]  && file=$(fd | dmenu -i -l 10)
 
 [[ ! -z $file ]] && curl -sF"file=@$file" $url | xclip -selection c && notify-send "Uploaded!"
 
